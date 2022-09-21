b3700074cfabe3dd161004cc86aa970f7a3a4eb0
Samartha S M <samarthasm@users.noreply.github.com>
2022-09-21 19:18:04 +0530

    Adding config files

---
+++ vars.tf
@@ -0,0 +1,26 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "Azuredevops"
+}
+
+variable "environment"{
+  description = "The environment should be used for all resources in this example"
+  default = "test"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "East US" 
+}
+
+variable "username"{
+  default = "odl_user_208142"
+}
+
+variable "password"{
+  default= "cisy58BWG*A2"
+}
+
+variable "packerImageId"{
+  default = "/subscriptions/ad6befd3-6a77-4714-b24d-181b38cb2753/resourceGroups/Azuredevops/providers/Microsoft.Compute/images/myPackerImage"
+}
